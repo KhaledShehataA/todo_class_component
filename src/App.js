702734b5a4d9448b0fb453b5527f0c96ae3146f5
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-04-27 23:35:37 +0200

    chore: Parent component App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,71 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/layout/Header';
 import Todos from './Components/Todos';
+import AddTodo from './Components/AddTodo';
+import uuid from 'uuid';
 
 class App extends Component {
-  state = {
-    todos: [
-      {
-        id: 1,
-        title: 'Take out the trash',
-        completed: true,
-      },
-      {
-        id: 2,
-        title: 'Dinner with wife',
-        completed: false,
-      },
-      {
-        id: 3,
-        title: 'Meeting with boss',
-        completed: false,
-      }
-    ]
-  }
+	state = {
+		todos: [
+			{
+				id: uuid.v4(),
+				title: 'Take out the trash',
+				completed: true,
+			},
+			{
+				id: uuid.v4(),
+				title: 'Dinner with wife',
+				completed: false,
+			},
+			{
+				id: uuid.v4(),
+				title: 'Meeting with boss',
+				completed: false,
+			},
+		],
+	};
 
-  markComplete = () => {
+	markComplete = (id) => {
+		this.setState({
+			todos: this.state.todos.map((todo) => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			}),
+		});
+	};
 
-  }
-  render() {
-    return(
-      <div>
-      <Todos todos ={this.state.todos} markComplete= {this.markComplete}/>
-      </div>
-    )
-  }
+	delTodo = (id) => {
+		this.setState({
+			todo: [...this.state.todos.filter((todo) => todo.id !== id)],
+		});
+	};
+
+	addTodo = (title) => {
+		const newTodo = {
+			id: uuid.v4(),
+			title,
+			completed: false,
+		};
+		this.setState({ todos: [...this.state.todos, newTodo] });
+	};
+	render() {
+		return (
+			<div className='App'>
+				<div className='container'>
+					<Header />
+					<AddTodo />
+					<Todos
+						todos={this.state.todos}
+						markComplete={this.markComplete}
+						delTodo={this.delTodo}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
